91f0eadf41b900b6c1db20a956abdc7d4d980425
Mikey <michaelp.esteban@gmail.com>
2020-12-06 13:06:19 -0500

    created RoleTitle starter, installed uuid

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 // import './App.css';
 import TailwindHW from './components/TailwindHW'
 import PersonalInfo from './components/PersonalInfo'
+import RoleTitle from './components/RoleTitle'
 
 function App() {
   return (
     <div className="App">
       <TailwindHW />
       <PersonalInfo />
+      <RoleTitle />
     </div>
   );
 }
